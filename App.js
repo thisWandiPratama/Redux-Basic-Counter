407064f787d9a6271424155db7093c33b1f4f923
OneBlog <wandi99pratama@gmail.com>
2019-10-19 19:24:07 +0700

    Redux Basic Counter

---
+++ App.js
@@ -0,0 +1,52 @@
+
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity
+} from 'react-native';
+
+import CounterApp from './src/counter/Counterapp'
+import {createStore} from 'redux'
+import { Provider } from 'react-redux'
+
+const nilaiAwal = {
+  Nilai : 0
+}
+
+const reducer = ( state = nilaiAwal, action) => {
+      switch(action.type){
+        case  'TAMBAHANGKA':
+        return {Nilai : state.Nilai + 1}
+        case 'KURANGANGKA' : 
+        return{Nilai : state.Nilai - 1}
+      }
+      return state
+}
+
+const store = createStore(reducer) //
+// Note :
+// Store
+// - Diambil dari halaman page ini terdapat fungsi-fungsi dari store, setelah terdapat action dan reducer. Di komponen ini yang terjadi adalah:
+// - Menyimpan state aplikasi;
+// - Mendapatkan akses ke dalam state, menggunakan getState();
+// - Dapat melakukan perubahan state menggunakan dispatch();
+// - Mendapatkan listener menggunakan subscribe(listener);
+// - Menangani listener yang belum teregistrasi dari balikan nilai darisubscribe(listener)
+
+class App extends React.Component{
+
+  render(){
+    return(
+        <Provider store={store}>
+         <CounterApp/>
+        </Provider>
+      )
+    }
+  }
+
+
+  export default App
+
---
+++ src/counter/Counterapp.js
@@ -0,0 +1,50 @@
+
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity
+} from 'react-native';
+
+import { connect} from 'react-redux'
+
+
+class CounterApp extends React.Component{
+
+
+  render(){
+    return(
+          <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
+            <View style={{flexDirection:'row', width: 200, justifyContent:'space-around'}}>
+            <TouchableOpacity onPress={() => this.props.TambahAngka()}>
+              <Text style ={{ fontSize: 20}}> Tambah </Text>
+            </TouchableOpacity>
+            <Text  style ={{ fontSize: 25}}>{this.props.Nilai}</Text>
+              <TouchableOpacity onPress={() => this.props.KurangAngka()}>
+              <Text style ={{ fontSize: 20}}> Berkurang </Text>
+            </TouchableOpacity>
+            </View>
+          </View>
+      )
+    }
+  }
+
+  function mapStateToProps(state){
+    return{
+      Nilai:state.Nilai
+    }
+  }
+
+  function mapDispatchToProps(dispatch){
+    return{
+      TambahAngka : () => dispatch({
+        type:'TAMBAHANGKA'
+      }),
+      KurangAngka : () => dispatch({
+        type:'KURANGANGKA'
+      })
+    }
+  }
+  export default connect(mapStateToProps, mapDispatchToProps)(CounterApp)
